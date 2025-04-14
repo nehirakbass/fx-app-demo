fcaa1f0aceff954eafa749657d86ff9a107eed87
nehirakbass <nehirakbas08@gmail.com>
2025-04-14 20:29:40 +0300

    initial commit

---
+++ src/main/java/com/fxapp/currency_conversion_demo/CurrencyConversionDemoApplication.java
@@ -0,0 +1,13 @@
+package com.fxapp.currency_conversion_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CurrencyConversionDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CurrencyConversionDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fxapp/currency_conversion_demo/CurrencyConversionDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fxapp.currency_conversion_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CurrencyConversionDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
