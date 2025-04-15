a163481a71125e6dc3b58ceac14a375d41951f29
nehirakbass <nehirakbas08@gmail.com>
2025-04-15 22:04:47 +0300

    fixed code for ResultCodes

--- src/main/java/com/fxapp/currencyconversion/constants/ResultCode.java
+++ src/main/java/com/fxapp/currencyconversion/constants/ResultCode.java
@@ -4,20 +4,19 @@ import org.springframework.http.HttpStatus;
 
 public enum ResultCode {
   INVALID_CURRENCY("01", "One or both currency codes are invalid", HttpStatus.BAD_REQUEST),
-  INVALID_AMOUNT("02", "Amount must be greater than 0", HttpStatus.BAD_REQUEST),
   EXTERNAL_API_ERROR(
-      "03", "Failed to fetch exchange rate from provider", HttpStatus.SERVICE_UNAVAILABLE),
+      "02", "Failed to fetch exchange rate from provider", HttpStatus.SERVICE_UNAVAILABLE),
   INTERNAL_SERVER_ERROR("99", "Unexpected error occurred", HttpStatus.INTERNAL_SERVER_ERROR),
-  NULL_CURRENCY_CODE("04", "Null currency code", HttpStatus.BAD_REQUEST),
+  NULL_CURRENCY_CODE("03", "Null currency code", HttpStatus.BAD_REQUEST),
   AMOUNT_MUST_BE_BIGGER_THAN_ZERO(
-      "05", "Amount value must be bigger than zero", HttpStatus.BAD_REQUEST),
+      "04", "Amount value must be bigger than zero", HttpStatus.BAD_REQUEST),
   AT_LEAST_ONE_FILTER_REQUIRED(
-      "08",
+      "05",
       "At least one filter (transactionId or transactionDate) must be provided",
       HttpStatus.BAD_REQUEST),
   FILE_PARSING_FAILED(
-      "09", "Parsing the bulk request file failed", HttpStatus.INTERNAL_SERVER_ERROR),
-  FILE_FORMAT_NOT_SUPPORTED("10", "Given file format is not supported", HttpStatus.BAD_REQUEST);
+      "06", "Parsing the bulk request file failed", HttpStatus.INTERNAL_SERVER_ERROR),
+  FILE_FORMAT_NOT_SUPPORTED("07", "Given file format is not supported", HttpStatus.BAD_REQUEST);
 
   private final String code;
   private final String message;
